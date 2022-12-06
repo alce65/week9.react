657b23652ccbd06bf42d8cf1bfbbfec8f4c52e3d
Alejandro Cerezo <alce65@hotmail.es>
2022-12-06 23:18:03 +0100

    Update initial setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test('renders Robots title', () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+    const element = screen.getByText(/robots/i);
+    expect(element).toBeInTheDocument();
 });
