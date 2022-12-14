a294c32bf140fdf7325459c680a1812e38510f03
Alejandro Cerezo <alce65@hotmail.es>
2022-12-15 00:00:38 +0100

    CSS reorganization in core components

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,5 +1,3 @@
-// Incluido en el proyecto inicial de React
-// import logo from './logo.svg';
 import './header.css';
 
 export function Header({ children }: { children: JSX.Element }) {
@@ -7,9 +5,6 @@ export function Header({ children }: { children: JSX.Element }) {
 
     return (
         <header className="header__main">
-            {/* Incluido en el proyecto inicial de React
-            <img src={logo} className="App-logo" alt="logo" />
-            */}
             <h1 className="header__title">{title}</h1>
             <div className="robot-menu">{children}</div>
         </header>
